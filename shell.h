9fd66b05cf3cc6da429f1b29aa5dbc6ef69840c3
luis-david <luissmith@hotmail.fr>
2024-04-25 19:18:23 +0200

    shell.c

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define  MAX_SIZE "1024"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+void tokenize(char *string, char *delim, char **arguments, int *count)
+	void free_arguments(char **arguments, int count)
+	int main( int argc, char **argv)
+#endif
